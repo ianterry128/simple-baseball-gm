97f9ad65a3660ce807785cee67bd87eac56acd17
Ian Perry <ianterry128@gmail.com>
2024-01-10 13:30:23 -0600

    can now view all teams in league

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 import { useState } from "react";
 import { GenerateTeam } from "~/components/GenerateTeam";
 import { lastNames } from "~/data/names";
+import { teamNames } from "~/data/names";
 
 import { api } from "~/utils/api";
 
 interface PlayerStateStruct {
-    name: String,
+    id: string,
+    name: string,
     age: number,
     strength: number,
     strengthPot: number,
@@ -27,98 +29,199 @@ interface PlayerStateStruct {
     classLvl: number
 }
 
+interface TeamStateStruct {
+  id: string,
+  name: string,
+  players: PlayerStateStruct[]
+}
+
+interface LeagueStateStruct {
+  id: string,
+  name: string,
+  teams: TeamStateStruct[]
+}
+
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
-  const [playerName, setPlayerName] = useState("John Doe");
-  const [playerInfo, setPlayerInfo] = useState<PlayerStateStruct[]>([]);
+  //const [playerInfo, setPlayerInfo] = useState<PlayerStateStruct[]>([]);
+  //const [teamInfo, setTeamInfo] = useState<TeamStateStruct>();
+  const [leagueInfo, setLeagueInfo] = useState<LeagueStateStruct>({
+    id: '',
+    name: '',
+    teams: []
+  });
+  const [selectedTeam, setSelectedTeam] = useState(0)
 
-// FUNCTIONS HERE USE REACT HOOKS
-  function displayTeam () {
-    let newPlayers: PlayerStateStruct[] = [];
-    const numPlayers: number = 9;
-    let n = 0;
-    while (n < numPlayers)
-    {
-      let newPlayer: PlayerStateStruct = {
-        name: '',
-        age: 0,
-        strength: 0,
-        strengthPot: 0,
-        speed: 0,
-        speedPot: 0,
-        precision: 0,
-        precisionPot: 0,
-        contact: 0,
-        contactPot: 0,
-        class: '',
-        potential: 0,
-        experience: 0,
-        level: 0,
-        classExp: 0,
-        classLvl: 0
-      }
-      
-      const _name = generateName();
-      console.log(_name);
-      newPlayer.name = _name;
-      newPlayer.age = Math.floor(Math.random() * (40 - 16) + 16);
-      newPlayer.strength = Math.floor(Math.random() * (30 - 1) + 1);
-
-      newPlayer.speed = Math.floor(Math.random() * (30 - 1) + 1);
-
-      newPlayer.precision = Math.floor(Math.random() * (30 - 1) + 1);
-
-      newPlayer.contact = Math.floor(Math.random() * (30 - 1) + 1);
-
-      const classesToGen: string[] = [
-        '1B',
-        '2B',
-        'SS',
-        '3B',
-        'CF',
-        'LF',
-        'RF',
-        'C',
-        'P'
-      ]; 
-      newPlayer.class = classesToGen[n];
-      newPlayers[n] = newPlayer;
-      n++;
+  function setSelectedTeamById(_id: string) {
+    let i: number = 0;
+    while (i < leagueInfo.teams.length && leagueInfo.teams[i]?.id !== _id) {
+      i++;
     }
-    setPlayerInfo(newPlayers); 
+    setSelectedTeam(i);
   }
 
-  /*function refreshFunc(){
-    const currentName = typeof window !== 'undefined' ? localStorage.getItem("_playerName") : playerName;
-    if (typeof currentName === 'string')
+// FUNCTIONS HERE USE REACT HOOKS
+  function createLeague () {
+    setSelectedTeam(1);
+    const numTeams: number = 6;
+    let m: number = 0;
+    let teamsToAdd: TeamStateStruct[] = [];
+    let teamNamesUsed: string[] = [];
+    while (m < numTeams)
     {
-      setPlayerName(currentName);
+      let newPlayers: PlayerStateStruct[] = [];
+      const numPlayers: number = 9;
+      let n = 0;
+      while (n < numPlayers)
+      {
+        let newPlayer: PlayerStateStruct = {
+          id: crypto.randomUUID(),
+          name: '',
+          age: 0,
+          strength: 0,
+          strengthPot: 0,
+          speed: 0,
+          speedPot: 0,
+          precision: 0,
+          precisionPot: 0,
+          contact: 0,
+          contactPot: 0,
+          class: '',
+          potential: 0,
+          experience: 0,
+          level: 0,
+          classExp: 0,
+          classLvl: 0
+        }
+        
+        const _name = generateName();
+        //console.log(_name);
+        newPlayer.name = _name;
+        newPlayer.age = Math.floor(Math.random() * (40 - 16) + 16);
+        newPlayer.strength = Math.floor(Math.random() * (30 - 1) + 1);
+
+        newPlayer.speed = Math.floor(Math.random() * (30 - 1) + 1);
+
+        newPlayer.precision = Math.floor(Math.random() * (30 - 1) + 1);
+
+        newPlayer.contact = Math.floor(Math.random() * (30 - 1) + 1);
+
+        const classesToGen: string[] = [
+          '1B',
+          '2B',
+          'SS',
+          '3B',
+          'CF',
+          'LF',
+          'RF',
+          'C',
+          'P'
+        ]; 
+        newPlayer.class = classesToGen[n];
+        newPlayers[n] = newPlayer;
+        n++;
+      }
+      // ensure there are no duplicate team names
+      let _teamName = generateTeamName();
+      while (teamNamesUsed.includes(_teamName)) {
+        _teamName = generateTeamName();
+      }
+      let teamToAdd: TeamStateStruct = {
+        id: crypto.randomUUID(),
+        name: _teamName,
+        players: newPlayers
+      }
+      teamsToAdd[m] = teamToAdd;
+      teamNamesUsed[m] = _teamName;
+    
+      /*
+      // store info in React state
+      setPlayerInfo(newPlayers); 
+      setTeamInfo(teamToAdd);
+
+      //store info in localstorage
+      if (typeof window !== 'undefined') {
+        localStorage.setItem("_playerInfo", JSON.stringify(newPlayers));
+      }
+      */
+      m++;
     }
-  }*/
+
+    let newLeague: LeagueStateStruct = {
+      id: crypto.randomUUID(),
+      name: 'Simple League',
+      teams: teamsToAdd
+    }
+
+    // store info in React state
+    setLeagueInfo(newLeague);
+  }
+
+  function LeagueTeamsTable({leagueInfoProp} : {leagueInfoProp:LeagueStateStruct}) {
+    return (
+        <div>
+          <table className="table-auto border-2 border-spacing-2 p-8">
+            <caption>My League: {leagueInfoProp.name}</caption>
+            <thead>
+              <tr className="even:bg-gray-50 odd:bg-white">
+                <th>Name</th>
+                <th>Wins</th>
+                <th>Losses</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                leagueInfoProp.teams.map((index) => {
+                  return (
+                    <tr 
+                    key={index.id} 
+                    className="even:bg-green-200 odd:bg-gray-50 hover:bg-blue-600 hover:text-gray-50"
+                    onClick={() => {
+                      setSelectedTeamById(index.id);
+                    }}>
+                      <td>{index.name}</td>
+                      <td>0</td>
+                      <td>0</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+    )
+  }
 
   //const displayName = typeof window !== 'undefined' ? localStorage.getItem("_playerName") : "Jane Doe";
   return (
     <>
     <div className="flex flex-col">
       <h1 className="text-center text-2xl">Welcome to Simple Baseball GM!</h1>
-      <button 
-            className="rounded-full transition-colors duration-200 hover:bg-green-500 
-        bg-green-700 text-white shadow-sm font-bold px-10 py-5 place-self-center"
-            onClick={() => displayTeam()}>
-            New League
-      </button>
-      <h1>{playerName}</h1>
-      
-        <li>name: {playerInfo[0]?.name} | position: {playerInfo[0]?.class}</li>
-        <li>name: {playerInfo[1]?.name} | Position: {playerInfo[1]?.class}</li>
-        <li>name: {playerInfo[2]?.name} | position: {playerInfo[2]?.class}</li>
-        <li>name: {playerInfo[3]?.name} | position: {playerInfo[3]?.class}</li>
-        <li>name: {playerInfo[4]?.name} | Position: {playerInfo[4]?.class}</li>
-        <li>name: {playerInfo[5]?.name} | position: {playerInfo[5]?.class}</li>
-        <li>name: {playerInfo[6]?.name} | position: {playerInfo[6]?.class}</li>
-        <li>name: {playerInfo[7]?.name} | position: {playerInfo[7]?.class}</li>
-        <li>name: {playerInfo[8]?.name} | Position: {playerInfo[8]?.class}</li>
-      
+      <div className="flex p-2">
+        <button 
+              className="rounded-full transition-colors duration-200 hover:bg-green-500 
+          bg-green-700 text-white shadow-sm font-bold px-10 py-5 w-52"
+              onClick={() => createLeague()}>
+              New League
+        </button>
+        <button 
+              className="rounded-full transition-colors duration-200 hover:bg-green-500 
+          bg-green-700 text-white shadow-sm font-bold px-10 py-5 w-52"
+              onClick={() => createLeague()}>
+              Exhibition
+        </button>
+      </div>
+      <div className="flex p-2 gap-4">
+        <TeamDisplayTable 
+        leagueInfoProp={leagueInfo}
+        teamIndexProp={0}
+        /> 
+        <TeamDisplayTable 
+        leagueInfoProp={leagueInfo}
+        teamIndexProp={selectedTeam}
+        /> 
+      </div>
+      <LeagueTeamsTable leagueInfoProp={leagueInfo} />   
     </div>
     <div className="flex">
     </div>  
@@ -141,3 +244,144 @@ function generateName(): string {
 
   return surName;
 }
+
+function generateTeamName(): string {
+  let _teamName: string = "";
+   
+  const randName = teamNames[Math.floor(Math.random() * teamNames.length)];
+  if (typeof randName === `string`)
+  {
+      _teamName = randName;
+  }
+
+  return _teamName;
+}
+
+function TeamDisplayTable({leagueInfoProp, teamIndexProp} : {leagueInfoProp:LeagueStateStruct, teamIndexProp:number}) {
+const captionText: string = teamIndexProp === 0 ? "My Team: " : "Opponent Team: "
+return (
+    <div>
+      <table className="table-auto border-2 border-spacing-2 p-8">
+        <caption>{captionText} {leagueInfoProp.teams[teamIndexProp]?.name}</caption>
+        <thead>
+          <tr className="even:bg-gray-50 odd:bg-white">
+            <th>Name</th>
+            <th>Class</th>
+            <th>Age</th>
+            <th>Str</th>
+            <th>Spd</th>
+            <th>Prc</th>
+            <th>Con</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            leagueInfoProp.teams[teamIndexProp]?.players.map((index) => {
+              return (
+                <tr key={index.id} className="even:bg-green-200 odd:bg-gray-50">
+                  <td>{index.name}</td>
+                  <td>{index.class}</td>
+                  <td>{index.age}</td>
+                  <td>{index.strength}</td>
+                  <td>{index.speed}</td>
+                  <td>{index.precision}</td>
+                  <td>{index.contact}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+)
+}
+
+/*
+function LeagueTeamsTable({leagueInfoProp} : {leagueInfoProp:LeagueStateStruct}) {
+  return (
+      <div>
+        <table className="table-auto border-2 border-spacing-2 p-8">
+          <caption>My League: {leagueInfoProp.name}</caption>
+          <thead>
+            <tr className="even:bg-gray-50 odd:bg-white">
+              <th>Name</th>
+              <th>Wins</th>
+              <th>Losses</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              leagueInfoProp.teams.map((index) => {
+                return (
+                  <tr 
+                  key={index.id} 
+                  className="even:bg-green-200 odd:bg-gray-50 hover:bg-blue-600 hover:text-gray-50"
+                  onClick={() => {
+                    console.log(`row click registered: ${index.name}`)
+                    //setSelectedTeam()
+                  }}>
+                    <td>{index.name}</td>
+                    <td>0</td>
+                    <td>0</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+  )
+}*/
+
+  /*
+  function populateTable() {
+    if (typeof _playerInfoLoc !== null)
+    {
+      _playerInfoLoc.map((index) => {
+        return (
+          <tr key={index.id}>
+            <td>{index.name}</td>
+            <td>{index.class}</td>
+            <td>{index.age}</td>
+            <td>{index.strength}</td>
+            <td>{index.speed}</td>
+            <td>{index.precision}</td>
+            <td>{index.contact}</td>
+          </tr>
+        )
+      })
+    }
+    else return (
+      <tr>
+            <td>blank</td>
+            <td>blank</td>
+            <td>blank</td>
+            <td>blank</td>
+            <td>blank</td>
+            <td>blank</td>
+            <td>blank</td>
+          </tr>
+    )
+  }
+  
+  notes: 
+  <TeamDisplayTable playerInfoProp={playerInfo}/>  
+
+  function TeamDisplayTable({playerInfoProp} : {playerInfoProp:PlayerStateStruct[]}) {
+
+    playerInfoProp.map((index) => {
+                return (
+                  <tr key={index.id} className="even:bg-green-200 odd:bg-gray-50">
+                    <td>{index.name}</td>
+                    <td>{index.class}</td>
+                    <td>{index.age}</td>
+                    <td>{index.strength}</td>
+                    <td>{index.speed}</td>
+                    <td>{index.precision}</td>
+                    <td>{index.contact}</td>
+                  </tr>
+                )
+              })
+  */
+
+
